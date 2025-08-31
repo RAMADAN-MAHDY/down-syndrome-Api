684aaa1ac9d7bffa9bfc2fb6435048e81084f591
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2025-08-31 11:04:39 +0300

    fix(cors): update origin URL to use HTTPS protocol
    The origin URL for the production environment was missing the HTTPS protocol, which could cause CORS issues. Updated to include the secure protocol for proper functionality.

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: ["http://localhost:5173"  , "down-syndrome-one.vercel.app"],
+    origin: ["http://localhost:5173"  , "https://down-syndrome-one.vercel.app"],
   optionsSuccessStatus: 200,
   credentials: true
 };
